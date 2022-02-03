4c6831baf4b466597af39a41af8d890a5b66b61a
nickswanson4 <88475946+nickswanson4@users.noreply.github.com>
2022-02-03 01:36:36 -0800

    Update ccnt.c

--- ccnt.c
+++ ccnt.c
@@ -8,17 +8,13 @@ int main(UNUSED int argc,  UNUSED char *argv[]) {
 	char buf[BUFSIZ];
 	int exitStatus = EXIT_SUCCESS;
 
-
 	while (fgets(buf, sizeof buf, stdin) != NULL) {
 
 		for (int i = 0; buf[i] != '\0'; i++) {
 			nchars++;
 		}
-		
-
 	}
 
 	printf("%8ld\n", nchars);
 	return exitStatus;
 }
-
